8a8b215c396648e4be4f9f2182244121a4bb3feb
Jonathan Berrisch <Jonathan@Berrisch.biz>
2020-08-30 15:22:18 +0200

    Check if container runs completely without init command

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -5,7 +5,7 @@ docker pull berrij/devenv:master
 
 # Grant xhost access to root users in order to start x11 devices (r plots)
 # Don't run in a codespaces environment see https://github.com/MicrosoftDocs/vscodespaces/issues/777
-# if [ "$CODESPACES" = false ]
-# then
-#     xhost local:root
-# fi
+if [ "$CODESPACES" = false ]
+then
+    xhost local:root
+fi
